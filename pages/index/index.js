00968b9174caf053b9d63fac0c570454340f9a99
Kathy <dengmeng04@163.com>
2019-05-09 20:27:38 +0800

    add main page news type selection function

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,7 @@
 const App = getApp()
 
 const typeMap = {
-  '国内': 'gn' ,
+  '国内': 'gn',
   '国际': 'gj',
   '财经': 'cj',
   '娱乐': 'yl',
@@ -13,18 +13,33 @@ const typeMap = {
 
 Page({
   data: {
-    newsType: ['国内','国际','财经','娱乐','军事','体育','其它'],
-   newsList: []
+    typeList: ['国内','国际','财经','娱乐','军事','体育','其它'],
+    type: 'gn',
+    mapType: '国内',
+    newsList: []
   },
   onLoad(){
+    this.getNews()
+  },
+  
+  onPullDownRefresh(){
+    this.getNews(()=>{
+      wx.stopPullDownRefresh()
+    })
+  },
+
+  getNews(callback){
     wx.request({
       url: 'https://test-miniprogram.com/api/news/list',
-      data:{
-        type: 'gn',
+      data: {
+        type: this.data.type
       },
-      success: res=>{
+      success: res => {
         let result = res.data.result
         this.setNewsList(result)
+      },
+      complete: ()=>{
+        callback && callback()
       }
     })
   },
@@ -36,6 +51,7 @@ Page({
       let date = new Date(result[i].date)
       // console.log(date.toLocaleTimeString())
       newsList.push({
+        id: result[i].id,
         title: result[i].title,
         source: result[i].source,
         date: `${date.getHours()}:${date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()}`,
@@ -45,6 +61,21 @@ Page({
     this.setData({
       newsList: newsList
     })
+  },
+  onTapType(event){
+    let type = event.currentTarget.dataset.item
+    this.setData({
+      type: typeMap[type],
+      mapType: type
+    })
+    this.getNews()
+  },
+  onTapNews(event){
+    console.log(event)
+    let id = event.currentTarget.dataset.id
+    console.log(id)
+    wx.navigateTo({
+      url: 'pages/detail/detail?id=' + id,
+    })
   }
-  
 })
